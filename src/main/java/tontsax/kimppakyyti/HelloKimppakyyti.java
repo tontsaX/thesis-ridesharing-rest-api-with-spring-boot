ddc24a1716bf0dbbb725bc915120ab2126d1280d
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-05-23 14:00:29 +0300

    Initialization

---
+++ src/main/java/tontsax/kimppakyyti/HelloKimppakyyti.java
@@ -0,0 +1,15 @@
+package tontsax.kimppakyyti;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//@Controller
+public class HelloKimppakyyti {
+
+//	@GetMapping("*")
+//	@ResponseBody
+//	public String hello() {
+//		return "Hello Kimppakyytisovellus!";
+//	}
+}
---
+++ src/test/java/tontsax/kimppakyyti/KimppakyytiApplicationTests.java
@@ -0,0 +1,13 @@
+package tontsax.kimppakyyti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimppakyytiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
