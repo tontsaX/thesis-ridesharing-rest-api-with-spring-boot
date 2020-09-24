a06c2eae70ec9030fd9684dd5db6b191330ce278
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-09-24 15:55:28 +0300

    Maintenance

--- src/main/java/tontsax/kimppakyyti/dao/RideDao.java
+++ src/main/java/tontsax/kimppakyyti/dao/RideDao.java
@@ -1,21 +1,18 @@
 package tontsax.kimppakyyti.dao;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RideDao extends JpaRepository<Ride, Long> {
+	
+	@EntityGraph(attributePaths = {"driver", "passengers"})
+	Ride getOne(Long id);
+	
 	List<Ride> findByOrigin(String origin);
 	List<Ride> findByDestination(String destination);
 	
-	// näitä tarvitsee tarkentaa jotenkin
-	// LocalDateTime-oliolla ei ole saanut tuloksia
-	// muutenkin tarvitsisi olla haku, jossa haetaan pvm perusteella eikä pvm ja kellonajalla
-//	List<Ride> findByDeparture(LocalDateTime departure);
-//	List<Ride> findByArrival(LocalDateTime arrival);
-//	List<Ride> findByDepartureLike(LocalDateTime departure);
-//	List<Ride> findByDepartureLike(String departure);
 	List<Ride> findByDepartureContaining(String departureDate);
 	List<Ride> findByArrivalContaining(String arrivalDate);
 }
