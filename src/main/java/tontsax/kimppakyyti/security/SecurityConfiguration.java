2b14bfb233a21b83f4c4de819eca1595f0124dbc
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-10-22 18:14:10 +0300

    Cleaning

--- src/main/java/tontsax/kimppakyyti/security/SecurityConfiguration.java
+++ src/main/java/tontsax/kimppakyyti/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeRequests()
 					.anyRequest().authenticated();
+					
 //        httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
 //		httpSecurity.csrf().disable();
 //		httpSecurity.headers().frameOptions().disable();
