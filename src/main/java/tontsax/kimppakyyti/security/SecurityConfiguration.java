bdd0db31a383beae12ec66dd8c8d8eb4431ecb8c
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-10-22 20:01:16 +0300

    All GET-requests are permit to all and any other request needs to be authenticated.

--- src/main/java/tontsax/kimppakyyti/security/SecurityConfiguration.java
+++ src/main/java/tontsax/kimppakyyti/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package tontsax.kimppakyyti.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,6 +10,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeRequests()
+					.antMatchers(HttpMethod.GET, "/rides").permitAll()
+//					.antMatchers(HttpMethod.POST, "/rides").authenticated();
 					.anyRequest().authenticated();
 					
 //        httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
