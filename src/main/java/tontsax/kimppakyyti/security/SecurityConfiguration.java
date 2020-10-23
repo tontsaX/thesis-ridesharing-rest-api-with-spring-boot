735f4057763b39a36fda491052a62cea0067cadf
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-10-23 15:41:41 +0300

    Loged in account can now be used by the controller

--- src/main/java/tontsax/kimppakyyti/security/SecurityConfiguration.java
+++ src/main/java/tontsax/kimppakyyti/security/SecurityConfiguration.java
@@ -1,18 +1,40 @@
 package tontsax.kimppakyyti.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.headers().frameOptions().sameOrigin();
+		
 		httpSecurity.authorizeRequests()
 					.antMatchers(HttpMethod.GET, "/rides/**").permitAll()
 					.anyRequest().authenticated().and()
 					.formLogin().permitAll().and()
 					.logout().permitAll();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder authManBuilder) throws Exception {
+		authManBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
