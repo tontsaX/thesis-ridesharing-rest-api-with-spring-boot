45bcfbc72c6b66f7617c61ccf1a23dee3f8bd017
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2021-02-18 16:05:12 +0200

    Moved all logic from AppController to specified services

--- src/main/java/tontsax/kimppakyyti/logic/AppController.java
+++ src/main/java/tontsax/kimppakyyti/logic/AppController.java
@@ -1,19 +1,9 @@
 package tontsax.kimppakyyti.logic;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,162 +14,79 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tontsax.kimppakyyti.dao.Account;
-import tontsax.kimppakyyti.dao.AccountDao;
 import tontsax.kimppakyyti.dao.Ride;
-import tontsax.kimppakyyti.dao.RideDao;
+import tontsax.kimppakyyti.services.AccountService;
+import tontsax.kimppakyyti.services.RideService;
 
 @RestController
 //@RequestMapping("/api") // lisää kaikkiin Mappingeihin alkuun osan /api
-public class AppController {
-
-	@Autowired
-	private RideDao rideDao;
+public class AppController {;
 	
 	@Autowired
-	private AccountDao accountDao;
+	private AccountService accountService;
 	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private RideService rideService;
 	
 	
 	@GetMapping("/rides")
 	public List<Ride> getRides(@RequestParam(defaultValue = "0") Integer page) {
-		return rideDao.findAll(getNextPageOrderByDeparture(page)).getContent();
+		return rideService.getRides(page);
 	}
 	
 	@GetMapping("/rides/from{origin}")
 	public List<Ride> getRidesByOrigin(@PathVariable String origin, @RequestParam(defaultValue = "0") Integer page) {
-		return rideDao.findByOrigin(origin, getNextPageOrderByDeparture(page));
+		return rideService.getRidesByOrigin(origin, page);
 	}
 	
 	@GetMapping("/rides/to{destination}")
 	public List<Ride> getRidesByDestination(@PathVariable String destination, @RequestParam(defaultValue = "0") Integer page) {
-		return rideDao.findByDestination(destination, getNextPageOrderByDeparture(page));
+		return rideService.getRidesByDestination(destination, page);
 	}
 	
 	@GetMapping("/rides/departure")
 	public List<Ride> getRidesByDeparture(@RequestParam String departure, @RequestParam(defaultValue = "0") Integer page) {
-		return rideDao.findByDepartureContaining(departure, getNextPageOrderByDeparture(page));
+		return rideService.getRidesByDeparture(departure, page);
 	}
 	
 	@GetMapping("/rides/arrival")
 	public List<Ride> getRidesByArrival(@RequestParam String arrival, @RequestParam(defaultValue = "0") Integer page) {
-		return rideDao.findByArrivalContaining(arrival, getNextPageOrderByDeparture(page));
-	}
-	
-	private Pageable getNextPageOrderByDeparture(Integer page) {
-		return PageRequest.of(page,10,Sort.by("departure").ascending());
+		return rideService.getRidesByArrival(arrival, page);
 	}
 	
 	@GetMapping("/rides/{id}")
 	public Ride getRide(@PathVariable Long id) {
-		return rideDao.getOne(id);
+		return rideService.getRide(id);
 	}
 	
 	@PostMapping("/rides")
 	public Ride postRide(@RequestBody String rideJson) throws JSONException {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(auth != null) {
-			Ride newRide = new Ride();
-			JSONObject receivedJson = new JSONObject(rideJson);
-			
-			newRide.setOrigin(receivedJson.getString("origin"));
-			newRide.setDestination(receivedJson.getString("destination"));
-			newRide.setPrice(receivedJson.getDouble("price"));
-			newRide.setDriver(accountDao.findByNickName(auth.getName()));
-			newRide.setDeparture(receivedJson.getString("departure"));
-			newRide.setArrival(receivedJson.getString("arrival"));
-			
-			return rideDao.save(newRide);
-		}
-
-		return Ride.EMPTY;
+		return rideService.createRide(rideJson);
 	}
 	
 	@PutMapping("/rides/{id}")
 	public List<Ride> addPassenger(@PathVariable Long id) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(auth != null) {
-			Account passenger = accountDao.findByNickName(auth.getName());
-			Ride ride = rideDao.getOne(id);
-			
-			passenger.getReservedRides().add(ride);
-			accountDao.save(passenger);
-			
-			return accountDao.getOne(passenger.getId()).getReservedRides();
-		}
-		
-		return Collections.emptyList();
+		return rideService.addPassenger(id);
 	}
 	
 	@PutMapping("/rides/{id}/cancel")
 	public List<Ride> cancelRide(@PathVariable Long id) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(auth != null) {
-			Account passenger = accountDao.findByNickName(auth.getName());
-			Ride ride = rideDao.getOne(id);
-			
-			passenger.getReservedRides().remove(ride);
-			accountDao.save(passenger);
-			
-			return accountDao.getOne(passenger.getId()).getReservedRides();
-		}
-		
-		return Collections.emptyList();
+		return rideService.cancelRide(id);
 	}
 	
 	@DeleteMapping("/account/rides/{id}")
 	public Boolean deleteRide(@PathVariable Long id) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(auth != null) {
-			Long accountId = accountDao.findByNickName(auth.getName()).getId();
-			Ride deletedRide = rideDao.getOne(id);
-			
-			if(deletedRide.getDriver().getId() == accountId) {
-				rideDao.deleteById(id);
-			}
-		}
-		
-		return !rideDao.existsById(id);
+		return rideService.deleteRide(id);
 	}
 	
 	@PutMapping("/account/rides/{id}")
 	public Ride updateRide(@RequestBody String rideJson, @PathVariable Long id) throws JSONException {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(auth != null) {
-			Long accountId = accountDao.findByNickName(auth.getName()).getId();
-			Ride updatedRide = rideDao.getOne(id);
-			
-			if(updatedRide.getDriver().getId() == accountId) {
-				JSONObject receivedJson = new JSONObject(rideJson);
-				
-				updatedRide.setOrigin(receivedJson.getString("origin"));
-				updatedRide.setDestination(receivedJson.getString("destination"));
-				updatedRide.setPrice(receivedJson.getDouble("price"));
-				updatedRide.setDeparture(receivedJson.getString("departure"));
-				updatedRide.setArrival(receivedJson.getString("arrival"));
-				
-				return rideDao.save(updatedRide);
-			}
-		}
-
-		return Ride.EMPTY;
+		return rideService.updateRide(rideJson, id);
 	}
 	
 	@PostMapping("/register")
 	public Account registerToApp(@RequestBody String accountJson) throws JSONException {
-		JSONObject receivedJson = new JSONObject(accountJson);
-		
-		Account newAccount = new Account();
-		newAccount.setNickName(receivedJson.getString("nickName"));
-		newAccount.setPassword(passwordEncoder.encode(receivedJson.getString("password")));
-		
-		return accountDao.save(newAccount);
+		return accountService.registerToApp(accountJson);
 	}
 	
 }
--- src/main/java/tontsax/kimppakyyti/services/AccountService.java
+++ src/main/java/tontsax/kimppakyyti/services/AccountService.java
@@ -5,5 +5,9 @@ import org.springframework.boot.configurationprocessor.json.JSONException;
 import tontsax.kimppakyyti.dao.Account;
 
 public interface AccountService {
+	public abstract Account getAccount(Long id);
+	public abstract Account getAccountByNickname(String nickName);
 	public abstract Account registerToApp(String accountJson) throws JSONException;
+	
+	public abstract Account save(Account account);
 }
